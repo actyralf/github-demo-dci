7344e774f4c467d072aa09736f92c293a3d31487
Thomas Engel <thomas.engel@dci-student.org>
2026-07-17 09:31:54 +0200

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import "./App.css";
 function App() {
   return (
     <>
+      <header>
+        <h1> New header (by Thomas) </h1>
+      </header>
+
       <main>
-        <h1>Github Demo</h1>
+        <h1>Github Project (by Thomas)</h1>
       </main>
     </>
   );
